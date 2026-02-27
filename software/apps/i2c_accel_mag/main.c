48f43df3cac8f04fddbfe9c9e1090b2f9681433b
Chloe Lu <chloelu2027@u.northwestern.edu>
2026-02-27 16:44:36 -0600

    update pins

--- software/apps/i2c_accel_mag/main.c
+++ software/apps/i2c_accel_mag/main.c
@@ -43,8 +43,8 @@ int main(void) {
   // These are NOT the correct pins for external I2C communication.
   // If you are using QWIIC or other external I2C devices, the are
   // connected to EDGE_P19 (a.k.a. I2C_QWIIC_SCL) and EDGE_P20 (a.k.a. I2C_QWIIC_SDA)
-  i2c_config.scl = I2C_INTERNAL_SCL;
-  i2c_config.sda = I2C_INTERNAL_SDA;
+  i2c_config.scl = I2C_QWIIC_SCL;
+  i2c_config.sda = I2C_QWIIC_SDA;
   i2c_config.frequency = NRF_DRV_TWI_FREQ_100K;
   i2c_config.interrupt_priority = 0;
   nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
