665e8b8de83a0f0dce75b9ab7704e0d7403ac1d9
Chloe Lu <chloelu2027@u.northwestern.edu>
2026-02-27 16:42:24 -0600

    setup

---
+++ software/apps/temp_humid/main.c
@@ -0,0 +1,66 @@
+// I2C accelerometer/magnetometer app
+//
+// Read from I2C accelerometer/magnetometer on the Microbit
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "nrf_delay.h"
+#include "nrf_twi_mngr.h"
+#include "app_timer.h"
+
+#include "microbit_v2.h"
+#include "lsm303agr.h"
+
+// Global variables
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 1, 0);
+APP_TIMER_DEF(timer_1);
+
+void print_reading() {
+  float temp = lsm303agr_read_temperature();
+  printf("Current temp: %f\n", temp);
+
+  lsm303agr_measurement_t accel = lsm303agr_read_accelerometer();
+  printf("acc\n x: %f, y: %f, z: %f\n", accel.x_axis, accel.y_axis, accel.z_axis);
+  printf("----------------\n");
+
+  lsm303agr_measurement_t mag = lsm303agr_read_magnetometer();
+  printf("mag\n x: %f, y: %f, z: %f\n", mag.x_axis, mag.y_axis, mag.z_axis);
+  printf("------------------------------\n");
+
+  float tilt = calculate_tilt(accel);
+  printf("tilt angle: %f\n", tilt);
+}
+
+int main(void) {
+  printf("Board started!\n");
+
+  // Initialize I2C peripheral and driver
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  // WARNING!!
+  // These are NOT the correct pins for external I2C communication.
+  // If you are using QWIIC or other external I2C devices, the are
+  // connected to EDGE_P19 (a.k.a. I2C_QWIIC_SCL) and EDGE_P20 (a.k.a. I2C_QWIIC_SDA)
+  i2c_config.scl = I2C_INTERNAL_SCL;
+  i2c_config.sda = I2C_INTERNAL_SDA;
+  i2c_config.frequency = NRF_DRV_TWI_FREQ_100K;
+  i2c_config.interrupt_priority = 0;
+  nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+
+  // Initialize the LSM303AGR accelerometer/magnetometer sensor
+  lsm303agr_init(&twi_mngr_instance);
+
+  //TODO: implement me!
+  app_timer_init();
+  app_timer_create(&timer_1, APP_TIMER_MODE_REPEATED, print_reading);
+  app_timer_start(timer_1, 32768, NULL);
+
+  // Loop forever
+  while (1) {
+    // Don't put any code in here. Instead put periodic code in a callback using a timer.
+    nrf_delay_ms(1000);
+  }
+}
+
---
+++ software/apps/temp_humid/temp_humid.c
@@ -0,0 +1,187 @@
+// LSM303AGR driver for Microbit_v2
+//
+// Initializes sensor and communicates over I2C
+// Capable of reading temperature, acceleration, and magnetic field strength
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "lsm303agr.h"
+#include "nrf_delay.h"
+#include "nrf_error.h"
+#include "nrf_twi_mngr.h"
+#include "sdk_errors.h"
+
+// Pointer to an initialized I2C instance to use for transactions
+static const nrf_twi_mngr_t* i2c_manager = NULL;
+
+// Helper function to perform a 1-byte I2C read of a given register
+//
+// i2c_addr - address of the device to read from
+// reg_addr - address of the register within the device to read
+//
+// returns 8-bit read value
+static uint8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr) {
+  uint8_t rx_buf = 0;
+  nrf_twi_mngr_transfer_t const read_transfer[] = {
+    NRF_TWI_MNGR_WRITE(i2c_addr, &reg_addr, 1, NRF_TWI_MNGR_NO_STOP),
+    NRF_TWI_MNGR_READ(i2c_addr, &rx_buf, 1, 0)
+  };
+  ret_code_t result = nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
+  if (result != NRF_SUCCESS) {
+    // Likely error codes:
+    //  NRF_ERROR_INTERNAL            (0x0003) - something is wrong with the driver itself
+    //  NRF_ERROR_INVALID_ADDR        (0x0010) - buffer passed was in Flash instead of RAM
+    //  NRF_ERROR_BUSY                (0x0011) - driver was busy with another transfer still
+    //  NRF_ERROR_DRV_TWI_ERR_OVERRUN (0x8200) - data was overwritten during the transaction
+    //  NRF_ERROR_DRV_TWI_ERR_ANACK   (0x8201) - i2c device did not acknowledge its address
+    //  NRF_ERROR_DRV_TWI_ERR_DNACK   (0x8202) - i2c device did not acknowledge a data byte
+    printf("I2C transaction failed! Error: %lX\n", result);
+  }
+
+  return rx_buf;
+}
+
+// Helper function to perform a 1-byte I2C write of a given register
+//
+// i2c_addr - address of the device to write to
+// reg_addr - address of the register within the device to write
+static void i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t data) {
+  //TODO: implement me
+  //Note: there should only be a single two-byte transfer to be performed
+  uint8_t msg[2] = {reg_addr, data};
+  nrf_twi_mngr_transfer_t write_transfer = NRF_TWI_MNGR_WRITE(i2c_addr, msg, 2, 0);
+
+  ret_code_t result = nrf_twi_mngr_perform(i2c_manager, NULL, &write_transfer, 1, NULL);
+  if (result != NRF_SUCCESS) {
+    printf("I2C transaction failed! Error: %lX\n", result);
+  }
+}
+
+// Initialize and configure the LSM303AGR accelerometer/magnetometer
+//
+// i2c - pointer to already initialized and enabled twim instance
+void lsm303agr_init(const nrf_twi_mngr_t* i2c) {
+  i2c_manager = i2c;
+
+  // ---Initialize Accelerometer---
+
+  // Reboot acclerometer
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG5_A, 0x80);
+  nrf_delay_ms(100); // needs delay to wait for reboot
+
+  // Enable Block Data Update
+  // Only updates sensor data when both halves of the data has been read
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG4_A, 0x80);
+
+  // Configure accelerometer at 100Hz, normal mode (10-bit)
+  // Enable x, y and z axes
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, CTRL_REG1_A, 0x57);
+
+  // Read WHO AM I register
+  // Always returns the same value if working
+  //TODO: read the Accelerometer WHO AM I register and check the result
+  uint8_t whoami = i2c_reg_read(LSM303AGR_ACC_ADDRESS, WHO_AM_I_A);
+  printf("WHOAMI returned %x\n", whoami);
+
+  uint8_t whoamim = i2c_reg_read(LSM303AGR_MAG_ADDRESS, WHO_AM_I_M);
+  printf("WHOAMIM returned %x\n", whoamim);
+
+  // ---Initialize Magnetometer---
+
+  // Reboot magnetometer
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_A_M, 0x40);
+  nrf_delay_ms(100); // needs delay to wait for reboot
+
+  // Enable Block Data Update
+  // Only updates sensor data when both halves of the data has been read
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_C_M, 0x10);
+
+  // Configure magnetometer at 100Hz, continuous mode
+  i2c_reg_write(LSM303AGR_MAG_ADDRESS, CFG_REG_A_M, 0x0C);
+
+  // Read WHO AM I register
+  //TODO: read the Magnetometer WHO AM I register and check the result
+
+  // ---Initialize Temperature---
+
+  // Enable temperature sensor
+  i2c_reg_write(LSM303AGR_ACC_ADDRESS, TEMP_CFG_REG_A, 0xC0);
+  nrf_delay_ms(100); // needs delay to be ready
+}
+
+// Read the internal temperature sensor
+//
+// Return measurement as floating point value in degrees C
+float lsm303agr_read_temperature(void) {
+  //TODO: implement me
+  uint8_t temp_l = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_TEMP_L_A);
+  uint8_t temp_h = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_TEMP_H_A);
+
+  int16_t temp = ((uint16_t)temp_h << 8) | temp_l;
+
+  float celsius = ((float)temp / 256.0) + 25.0;
+
+  return celsius;
+}
+
+lsm303agr_measurement_t lsm303agr_read_accelerometer(void) {
+  //TODO: implement me
+
+  uint8_t x_l = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_X_L_A);
+  uint8_t x_h = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_X_H_A);
+  uint8_t y_l = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_Y_L_A);
+  uint8_t y_h = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_Y_H_A);
+  uint8_t z_l = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_Z_L_A);
+  uint8_t z_h = i2c_reg_read(LSM303AGR_ACC_ADDRESS, OUT_Z_H_A);
+
+  int16_t x_raw = ((x_h << 8) | x_l) >> 6;
+  int16_t y_raw = ((y_h << 8) | y_l) >> 6;
+  int16_t z_raw = ((z_h << 8) | z_l) >> 6;
+
+  float x_scaled = (float) x_raw * 3.9 / 1000.0;
+  float y_scaled = (float) y_raw * 3.9 / 1000.0;
+  float z_scaled = (float) z_raw * 3.9 / 1000.0;
+
+  lsm303agr_measurement_t measurement = {
+    x_scaled,
+    y_scaled,
+    z_scaled
+  };
+  return measurement;
+}
+
+lsm303agr_measurement_t lsm303agr_read_magnetometer(void) {
+  //TODO: implement me
+  uint8_t x_l = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTX_L_REG_M);
+  uint8_t x_h = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTX_H_REG_M);
+  uint8_t y_l = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTY_L_REG_M);
+  uint8_t y_h = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTY_H_REG_M);
+  uint8_t z_l = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTY_L_REG_M);
+  uint8_t z_h = i2c_reg_read(LSM303AGR_MAG_ADDRESS, OUTY_H_REG_M);
+
+  int16_t x_raw = ((x_h << 8) | x_l);
+  int16_t y_raw = ((y_h << 8) | y_l);
+  int16_t z_raw = ((z_h << 8) | z_l);
+
+  float x_scaled = (float) x_raw * 1.5 / 10.0;
+  float y_scaled = (float) y_raw * 1.5 / 10.0;
+  float z_scaled = (float) z_raw * 1.5 / 10.0;
+
+  lsm303agr_measurement_t measurement = {    
+    x_scaled,
+    y_scaled,
+    z_scaled};
+
+  return measurement;
+}
+
+float calculate_tilt(lsm303agr_measurement_t accel) {
+  float numerator = sqrt(accel.x_axis*accel.x_axis + accel.y_axis*accel.y_axis);
+  float phi_rads = atan(numerator / accel.z_axis);
+  float phi_deg = phi_rads * 180 / M_PI;
+
+  return phi_deg;
+}
